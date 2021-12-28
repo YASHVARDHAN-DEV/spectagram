66221a935db70268d9c92917f44219f61f5ba413
YASHVARDHAN-DEV <80517622+YASHVARDHAN-DEV@users.noreply.github.com>
2021-12-28 10:51:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import DrawerNavigator from './navigation/DrawerNavigator';
+import DrawerNavigator from './Navigation/DrawerNavigator';
 
 export default function App() {
   return (
